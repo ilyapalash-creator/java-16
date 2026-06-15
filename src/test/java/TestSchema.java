58d9addaba658651a09628f3d0cfb7750c30348a
Ilya Palash <rudut.ilya@ya.ru>
2026-06-15 20:17:56 +0300

    Fallen construction

---
+++ src/test/java/TestSchema.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+public class TestSchema {
+    @Test
+    void shouldValidateAccountsSchema() {
+        given()
+                .baseUri("http://localhost:9999")  // порт SUT
+                .when()
+                .get("/api/accounts")
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+    }
+}
